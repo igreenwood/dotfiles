1892a458127be7b7b9c858467c7fac5459c82955
Daisuke Tsuji <dim0627@gmail.com>
2017-11-21 13:48:59 +0900

    remove extra files.

--- .lein/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-cljfmt "0.2.1"]]
-        :dependencies [[slamhound "1.5.5"]
-                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-                       [cljfmt "0.2.0"]]}}
