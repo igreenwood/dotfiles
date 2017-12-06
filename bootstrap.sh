87d002d1607a961ef895c336a4c41480d327262d
Daisuke Tsuji <dim0627@gmail.com>
2017-12-06 10:53:01 +0900

    improve bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,22 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+brew install wget
+brew install git
+brew install tree
+brew install tig
+brew install ag
+brew install jq
+brew install python3
+brew install neovim
+brew install peco
+brew install envchain
+
+pip3 install neovim
+
 ln -s $SCRIPT_DIR/.zshrc $HOME/.zshrc
 ln -s $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
 ln -s $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
